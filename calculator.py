9d858dcf0889af94551d86f5d7f4bbe060d50c1a
Arjun <ironarjun17@gmail.com>
2025-11-18 18:41:27 -0500

    fixed function names and errors

--- calculator.py
+++ calculator.py
@@ -2,28 +2,36 @@
 #Arjun Suresh
 #Luliana Mashchenko
 import math
+
 def add(a, b):
     return a + b
 
-def sub(a, b):
-    return a-b
+def subtract(a, b):
+    return a - b
 
-def mul(a, b):
-    return a*b
+def multiply(a, b):
+    return a * b
 
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b / a
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return a / b
 
-def log(a, b):
-    if a <= 0 or b<=0 or b ==1:
-        raise ValueError
-    return math.log(a,b)
+def logarithm(a, base):
+    if a <= 0 or base <= 0 or base == 1:
+        raise ValueError("Invalid input for logarithm.")
+    return math.log(a, base)
 
-def exp(a, b):
-    return a**b
+def exponent(a, b):
+    return a ** b
 
+def hypotenuse(a, b):
+    return math.sqrt(a*a + b*b)
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot compute square root of a negative number.")
+    return math.sqrt(a)
 
 
 
